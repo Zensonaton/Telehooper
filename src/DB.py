8d7954c565dc29c2c3169392db667c149b2aa763
Zensonaton <zensonaton@gmail.com>
2022-08-27 23:05:10 +0600

    Фикс подключения к ДБ.

--- src/DB.py
+++ src/DB.py
@@ -10,14 +10,14 @@ from pymongo.mongo_client import MongoClient
 import urllib.parse 
 
 
-def getDatabase(host: str = "localhost", port: int = 27017, user: str | None = None, pwd: str | None = None) -> MongoClient:
+def getDatabase(host: str = "localhost", port: int = 27017, user: str | None = None, pwd: str | None = None, auth_source: str | None = None) -> MongoClient:
 	"""
 	Пытается подключиться к MongoDB-базе данных.
 	"""
 
 	connectionUri = f"mongodb://{host}:{port}"
-	if pwd and user:
-		connectionUri = f"mongodb://{user}:{urllib.parse.quote(pwd, safe='')}@{host}:{port}/"
+	if pwd and user and auth_source:
+		connectionUri = f"mongodb://{user}:{urllib.parse.quote(pwd, safe='')}@{host}:{port}/?authSource={auth_source}"
 
 	return MongoClient(connectionUri)
 
@@ -37,7 +37,8 @@ def getDefaultDatabase() -> MongoClient:
 		host=os.environ["MONGODB_HOST"],
 		port=int(os.environ["MONGODB_PORT"]),
 		user=os.environ["MONGODB_USER"],
-		pwd=os.environ["MONGODB_PWD"]
+		pwd=os.environ["MONGODB_PWD"],
+		auth_source=os.environ["MONGODB_DBNAME"]
 	)
 
 def getDefaultCollection():
