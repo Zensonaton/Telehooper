e432831339187f1c724dfd7349ef9615698ac09f
Zensonaton <zensonaton@gmail.com>
2023-12-03 18:08:28 +0600

    Удаление ненужных переменных в ВК.

--- src/services/vk/consts.py
+++ src/services/vk/consts.py
@@ -5,12 +5,6 @@ from consts import COMMANDS_USERS_GROUPS_CONVERTED
 
 VK_OAUTH_URL = "https://oauth.vk.com/authorize?client_id=2685278&scope=69634&redirect_uri=https://oauth.vk.com/blank.html&display=page&response_type=token&revoke=1"
 """OAuth-ссылка для получения токена ВКонтакте."""
-VK_USERS_GET_AUTOUPDATE_SECS = 12 * 60 * 60 # 12 часов.
-"""Время в секундах, отображающее интервал для автообновления информации о пользователях."""
-VK_USER_INACTIVE_SECS = 7 * 24 * 60 * 60 # 7 дней.
-"""Время в секундах, отображающее интервал для определения неактивности пользователя. После этого времени пользователь считается неактивным, и информация о нём перестанет обновляться."""
-VK_USER_REMOVAL_SECS = 30 * 24 * 60 * 60 # 30 дней.
-"""Время в секундах, отображающее интервал для определения удаления кэшированной информации пользователя."""
 VK_INVISIBLE_CHARACTER = "&#12288;"
 """Невидимый символ, используемый для отображения пустых строк в сообщениях."""
 VK_GROUP_DIALOGUE_COMMANDS = {
